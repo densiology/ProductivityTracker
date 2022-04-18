8708095cbe4bfa2419d22d906c623fe77f8db883
Dennis Espiritu <dennis.e2k@gmail.com>
2022-04-18 14:08:28 +0800

    Added dependencies.

---
+++ app/src/main/java/com/dennis/productivitytracker/ProdTrackerApp.kt
@@ -0,0 +1,9 @@
+package com.dennis.productivitytracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ProdTrackerApp : Application() {
+
+}
---
+++ app/src/main/java/com/dennis/productivitytracker/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.dennis.productivitytracker.di
+
+import android.app.Application
+import androidx.room.Room
+import com.dennis.productivitytracker.data.ProdTrackerDB
+import com.dennis.productivitytracker.data.ProdTrackerRepo
+import com.dennis.productivitytracker.data.ProdTrackerRepoImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideProdTrackerDB(app: Application): ProdTrackerDB {
+        return Room.databaseBuilder(
+            app,
+            ProdTrackerDB::class.java,
+            "prod_tracker_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProdTrackerRepo(prodTrackerDB: ProdTrackerDB): ProdTrackerRepo {
+        return ProdTrackerRepoImpl(prodTrackerDB.prodTrackerDao)
+    }
+}

ce52102ef333663cba698c47aa2e45ccdcfad77e
Dennis Espiritu <dennis.e2k@gmail.com>
2022-04-18 12:58:57 +0800

    Combined daos.

--- app/src/main/java/com/dennis/productivitytracker/data/ProdTrackerDB.kt
+++ app/src/main/java/com/dennis/productivitytracker/data/ProdTrackerDB.kt
@@ -2,8 +2,6 @@ package com.dennis.productivitytracker.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.dennis.productivitytracker.data.dao.CommonTaskDao
-import com.dennis.productivitytracker.data.dao.TaskDao
 import com.dennis.productivitytracker.data.entities.CommonTaskEntity
 import com.dennis.productivitytracker.data.entities.TaskEntity
 
@@ -11,7 +9,7 @@ import com.dennis.productivitytracker.data.entities.TaskEntity
     entities = [TaskEntity::class, CommonTaskEntity::class],
     version = 1
 )
-abstract class ProductivityTrackerDB : RoomDatabase() {
-    abstract val taskDao: TaskDao
-    abstract val commonTaskDao: CommonTaskDao
+
+abstract class ProdTrackerDB : RoomDatabase() {
+    abstract val prodTrackerDao: ProdTrackerDao
 }
--- app/src/main/java/com/dennis/productivitytracker/data/ProdTrackerDao.kt
+++ app/src/main/java/com/dennis/productivitytracker/data/ProdTrackerDao.kt
@@ -1,17 +1,27 @@
-package com.dennis.productivitytracker.data.dao
+package com.dennis.productivitytracker.data
 
 import androidx.room.*
 import com.dennis.productivitytracker.data.entities.CommonTaskEntity
+import com.dennis.productivitytracker.data.entities.TaskEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface CommonTaskDao {
+interface ProdTrackerDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: TaskEntity)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCommonTask(commonTask: CommonTaskEntity)
 
     @Delete
+    suspend fun deleteTask(task: TaskEntity)
+
+    @Delete
     suspend fun deleteCommonTask(commonTask: CommonTaskEntity)
+
+    @Query("SELECT * FROM TaskEntity")
+    fun getTasks(): Flow<List<TaskEntity>>
 
     @Query("SELECT * FROM CommonTaskEntity")
     fun getCommonTasks(): Flow<List<CommonTaskEntity>>
